989ff4cb0a8569885b6bf07969bec862c93b7f92
Jhamil arroyo <jhamil.arroyo.a@gmail.com>
2022-02-21 22:32:50 -0500

    javascript asincrono se me complico un poco

---
+++ js/constantes.js
@@ -0,0 +1,8 @@
+export const PI = Math.PI;
+
+export let user = "Jhamil";
+let password = "qwerty";
+
+export default function Saluda() {
+    console.log("Hola Módulos +ES6");
+}
---
+++ js/modulos.js
@@ -0,0 +1,10 @@
+import Saluda, {PI,user} from "./constantes.js"
+import {aritmetica} from "./aritmetica.js"
+
+
+console.log("Archivo modulos.js");
+console.log(PI,user);
+console.log(aritmetica.sumar(3,5));
+console.log(aritmetica.restar(9,2));
+Saluda();
+
